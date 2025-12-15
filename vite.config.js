a4eda481d148469a1acb64672d38682201fff27b
1Edier <223228@ids.upchiapas.edu.mx>
2025-12-15 04:13:45 -0600

    vite config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    port: 5173
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: process.env.PORT || 4173,
+    allowedHosts: [
+      'front-cacei.onrender.com',
+      '.onrender.com'
+    ]
+  }
 })
